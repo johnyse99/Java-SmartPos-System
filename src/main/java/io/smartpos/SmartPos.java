8eb83b3a067537766a9971103a0d748b96a345b0
Juan S. <regiomaster@gmail.com>
2026-01-11 00:37:26 -0800

    Add files via upload

---
+++ src/main/java/io/smartpos/SmartPos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.smartpos;
+
+import io.smartpos.ui.MainApp;
+
+/**
+ *
+ * @author DEVELOPER
+ */
+public class SmartPos {
+
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
